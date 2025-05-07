130a12e41e7b01f9ae5c6655524f80714e01a3df
mterrera29 <matiasterrera29@gmail.com>
2025-05-07 09:55:21 -0300

    py

--- pruebas.py
+++ pruebas.py
@@ -1,4 +1,21 @@
-y= 5
-y *= 2
+#def cuentaAtras(n):
+#  while n>0:
+#    #"Mientras n sea mayor que 0 mostrar el valor de n y luego decrementar n. Cuandose llegue a 0 mostrar It's the final countdown".
+#    print(n)
+#    n -= 1
+#  print("It's the final countdown",n)
+#  
+#
+#cuentaAtras(20)
 
-print(y)
+#for i in "Hola":
+#  print(i)
+#  if i== "o":
+#    break
+
+#x = 9
+#print(x % 2)
+#
+#listal = [20,40,10,40,50]
+#print(len(listal))
+
