0dccac67b9e122f86a0cc533b28927958cee0186
mterrera29 <matiasterrera29@gmail.com>
2025-05-17 09:57:44 -0300

    ts

--- semana_6.py
+++ semana_6.py
@@ -47,4 +47,37 @@
 #  result = num * i
 #  print(f"{num} x {i} = {result}")
 #  i += 1
- 
+
+#3
+
+#num = int(input("introduce un numero de amigos a invitar"))
+#
+#if num <= 10:
+#  for i in range(0,num):
+#    amigo = input("introduce el nombre de tu amigo: ")
+#    print(f"✔ {amigo} invitado.")
+#else:
+#  print("Demasiada gente")
+
+#4
+##Inicializar total a 0, luego mientras total sea menor o igual a 50 pida al usuario que ingrese
+#un número. Sume ese número al total e imprima el mensaje "El total es [total]". Deténgase
+#cuando el total supere a 50.
+total = 0
+
+#if total <= 50:
+#  while total < 50:
+#    num = int(input("introduce un numero "))
+#    total += num
+#    print(f"El total es {total}")
+
+#5
+#Un número perfecto es un entero positivo, que es igual a a la suma de todos los enteros
+#positivos (excluido el mismo) que son divisores del número. El primer número perfecto es
+#6, ya que los divisores de 6 son 1, 2, 3, y 1 + 2 + 3 = 6.
+#Para encontrar el máximo común divisor (mcd) de dos números se emplea el algoritmo de
+#Euclides, que se puede describir así: Dados los enteros a y b (a > b), se divide a por b,
+#obteniendo el cociente q1 y el resto r1. Si r1 != 0, se divide r por b1, obteniendo el cociente
+#q2 y el resto r2. Si r2 != 0, se divide r1 por r2, para obtener q1 y r3 y así sucesivamente. Se
+#continúa el proceso hasta que se obtiene un resto 0 . El resto anterior es entonces el mcd
+#de los números a y b. Escribir un programa que calcule el mcd de dos números.
--- tp2.py
+++ tp2.py
@@ -1,65 +1,126 @@
+#1. Que al pasarle una cadena <nombre> nos muestre por pantalla el saludo ¡Hola <nombre>!.
 
-#1
-#num1 = float(input("Ingresar un numero:"))
-#num2 = float(input("Ingresar otro numero:"))
-#suma = num1 + num2
-#print(suma)
-#num3 = float(input("Ingresar otro numero:"))
-#operacion = num3/suma
-#print(operacion)
+#num=(input("introduce tu nombre: "))
+#def saludar(nom):
+#  print(f"Hola {nom}!")
+#  
+#saludar(num)
 
-#2
-#num1 = int(input("Ingresar un numero:"))
-#print(num1 % 2 == 0)
- 
-#3 
-#text = input("Ingresar un texto:")
-#numText = int(len(text))
-#print(numText % 2 == 0)
 
-#4
-#tarifa_horaria = float(input("Ingresar la tarifa horaria:"))
-#horas_trabajadas = float(input("Ingresar las horas trabajadas por día:"))
-#salario = (tarifa_horaria * horas_trabajadas) *5
-#print(f"El salario semanal es: {salario}$")
 
-#5
-#num1 = int(input("Ingresar un numero de tres digitos:"))
-#num2 = int(input("Ingresar otro numero de tres digitos:"))
-#resultado= num1 * num2
-#unidad= int(str(num2)[-1])
-#decena= int(str(num2)[-2])
-#centena= int(str(num2)[-3])
-#print(f"  {num1}\n* {num2}\n______\n{num1*unidad}\n{num1*decena}\n{num1*centena}\n{num1*num2}")
+#2. Que reciba un número entero positivo y una potencia a elevar y que nos devuelva el
+#resultado.
 
-#6
-#c1 =int(input("Ingresar un lado del triangulo:"))
-#c2=int(input("Ingresar otro lado del triangulo:"))
+#100numero=int(input("introduce un numero positivo: "))
+#100potencia=int(input("introduce su potencia: "))
+#100
+#100def funcion(num, pot):
+#100  if num < 0:
+#100    print("el número debe ser positivo")
+#100    return
+#100  resultado= numero ** potencia
+#100  print(resultado)
+#100  
+#100funcion(numero,potencia)
+
+#3. Que nos calcule el total de una factura tras aplicarle el IVA. La función debe recibir la
+#cantidad sin IVA y el porcentaje de IVA a aplicar, y devolver el total de la factura. Si se invoca
+#la función sin pasarle el porcentaje de IVA, deberá aplicar un 21%.
+
+#totalFactura=int(input("introduce el total de la factura: "))
+#IVA=int(input("introduce porcentaje de IVA: "))
 #
-#hipotenusa = (c2**2+c1**2)**0.5
-#print(f"la hipotenusa es {hipotenusa}")
-
-#7
-#def esMayor (num1,num2):
-#  if num1 > num2:
-#    return"Es mayor"
+#def funcion(total, impuestos =21):
+#  if (impuestos == 21):
+#    resultado= total * (21 /100)
+#    print(total +resultado)
 #  else:
-#    return"No es mayor"
-#print(esMayor(9,5))
+#    resultado= total * (impuestos /100)
+#    print(total +resultado)
+#  
+#funcion(totalFactura)
 
-#8
-#def promedio (num1,num2,num3):
-#  operacion = (num1 + num2 +num3)/3
-#  return operacion
-#print(promedio(3,4,6))
+#4. Que dada la base y altura de un triángulo nos calcule su área.
 
-#9
+#base=int(input("introduce el total de la factura: "))
+#altura=int(input("introduce el total de la factura: "))
+#def area(base, altura):
+#  areaTriangulo = base*altura
+#  print(f"el area del triangulo es {areaTriangulo}")
+#  
+#area(base,altura)
+  
+#5. Que dado tres números ingresados por el usuario nos devuelva el promedio.
 
-#def imprimirCaracteres (cant):
-#  return "*"*cant
-#print(imprimirCaracteres(20))
+#num1=int(input("introduce un num: "))
+#num2=int(input("introduce un num: "))
+#num3=int(input("introduce un num: "))
+#def promedio(num1, num2, num3):
+#  promedio = (num1+num2+num3)/3
+#  print(f"el promedio es {promedio}")
+#  
+#promedio(num1, num2, num3)
 
-def imprimir_nombre(nombre):
-    print(nombre)
+#En esta segunda parte deberán utilizar estructuras de control condicionales (y en lo posible y dado el
+#caso funciones) para escribir programas que lleven a cabo lo siguiente:
+#6. Que pida al usuario una palabra y la muestre por pantalla 10 veces.
 
-imprimir_nombre('Programación 1 ' * 4)
+#palabra=input("ingrese una palabra: ")
+#def repetirPalabra(palabra):
+#  print(palabra*10)
+#  
+#repetirPalabra(palabra)
+
+#7. Que imprima todos los números entre el 100 y el 199.
+#num = 100
+#while num <= 199:
+#  print(num)
+#  num = num+1
+#
+
+#8. Pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+#edad=int(input("ingrese su edad: "))
+#
+#if edad >= 18:
+#  print("es mayor")
+#else:
+#  print("es menor")
+
+#9. Que el usuario ingrese dos números y muestre cuál de los dos es menor. Considerar el caso
+#en que ambos números son iguales.
+#num1=int(input("ingrese un numero: "))
+#num2=int(input("ingrese otro numero: "))
+#
+#if num1 > num2:
+#  print(f"{num2} es menor")
+#elif num1 < num2:
+#  print(f"{num1} es menor")
+#else:
+#  print("son iguales")
+
+#10. Que el usuario ingrese un número entero positivo y muestre por pantalla lo siguiente:
+#a. Todos los números impares desde 1 hasta ese número separados por comas.
+#b. La cuenta atrás desde ese número hasta cero separados por comas.
+#c. Que indique si es primo o no.
+#d. Por último, su factorial.
+
+num1=int(input("ingrese un numero entero positivo "))
+i = 0
+impares = ""
+
+while i <= num1:
+  if i %2 == 0:
+    i = i+1
+  else:
+    impares = impares + str(i)+","
+    i = i+1 
+    
+cuentaAtras = ""
+
+x= num1
+while x >= 0:
+  cuentaAtras = cuentaAtras + str(x)+","
+  x= x-1 
+    
+print(impares)
+print(cuentaAtras)
