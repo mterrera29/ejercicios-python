9d5b2445cc5f1e536118112164bd0c0059f1343a
mterrera29 <matiasterrera29@gmail.com>
2025-08-24 11:26:49 -0300

    fetch

--- prog 2/semana3/mainNave.py
+++ prog 2/semana3/mainNave.py
@@ -4,3 +4,4 @@ from naveEspacial import *
 nave_espacial1 = NaveEspacial('R', 500)
 nave_espacial2 = NaveEspacial('R', 500)
 print('Nave 1 = Nave 2: ' + str(nave_espacial1 == nave_espacial2))
+print(nave_espacial1,nave_espacial2)
