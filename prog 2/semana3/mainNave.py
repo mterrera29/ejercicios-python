b31e4c99ef35dd706ce0354e853516e003ca1a0e
mterrera29 <matiasterrera29@gmail.com>
2025-08-24 11:26:31 -0300

    fetch

--- prog 2/semana3/mainNave.py
+++ prog 2/semana3/mainNave.py
@@ -1,13 +1,6 @@
 from naveEspacial import *
 
-nave_espacial1 = NaveEspacial('R', 500)
-print('Combustible de Nave 1: ' + str(nave_espacial1.obtenerCombustible()))
-nave_espacial1.establecerEstadoAlertas(True)
-nave_espacial1.agregarCombustible(700)
-print('Combustible de Nave 1: ' + str(nave_espacial1.obtenerCombustible()))
 
-nave_espacial2 = NaveEspacial ('A', 0)
-print('Combustible de Nave 2: ' + str(nave_espacial2.obtenerCombustible()))
-nave_espacial2.establecerEstadoAlertas
-nave_espacial2.agregarCombustible(200)
-print('Combustible de Nave 2: ' + str(nave_espacial2.obtenerCombustible()))
+nave_espacial1 = NaveEspacial('R', 500)
+nave_espacial2 = NaveEspacial('R', 500)
+print('Nave 1 = Nave 2: ' + str(nave_espacial1 == nave_espacial2))
