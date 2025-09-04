bd8accb32838f64a304b0c32c23e970386bd6fac
mterrera29 <matiasterrera29@gmail.com>
2025-09-04 11:54:15 -0300

    fetch

--- prog 2/tp2/tp2b/main.py
+++ prog 2/tp2/tp2b/main.py
@@ -3,3 +3,11 @@ from cancion import *
 cancion1 = Cancion("Adios",350,"rock")
 cancion2 = Cancion("Crimen",250,"rock")
 cancion3 = Cancion("Primavera 0",410,"rock")
+
+print(cancion1.obtenerGenero())
+print(cancion2.obtenerGenero())
+print(cancion3.obtenerGenero())
+
+cancion2.establecerGenero("bolero")
+
+print(cancion2.obtenerGenero())
