f03496eaf6b97fca8cde461f098754dbde17b6ee
mterrera29 <matiasterrera29@gmail.com>
2025-09-30 17:30:44 -0300

    fetch

---
+++ prog 2/tp3/a/estanteria.py
@@ -0,0 +1,21 @@
+class Estanteria():
+ # Atributos de clase
+  numeroSiguiente= 0
+  
+  # Método de inicialización
+  def __init__(self):
+    self.numero = self.numeroSiguiente +1
+  # Atributos de instancia
+    
+  
+  #Comandos
+  def agregarCaja(self, caja):
+  
+  def removerCaja(self, caja):
+ 
+  #Consultas
+  def obtenerNumero(self):
+    return self.numero
+    
+  def obtenerEstanterias(self):
+    return self.descripcion 
