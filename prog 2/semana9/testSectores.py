6ab1a68d4600a3df3a2dccd3bd6a3e59af112deb
mterrera29 <matiasterrera29@gmail.com>
2025-10-14 20:02:15 -0300

    fetch

--- prog 2/semana9/testSectores.py
+++ prog 2/semana9/testSectores.py
@@ -4,9 +4,9 @@ from sectoresFabrica import SectoresFabrica
 class TestPlanta():
     def main(self):
       fabrica = SectoresFabrica(2)
-      r1 = Robot('Robot 1')
-      r2 = Robot('Robot 2')
-      print(fabrica.existeSector(2))
+      fabrica.asignar(Robot('Robot 1'), 0)
+      fabrica.asignar(Robot('Robot 1'), 1)
+      print(fabrica.robotSector(0) == fabrica.robotSector(1))
 
 if __name__ == '__main__':
     test = TestPlanta()
